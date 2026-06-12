3b3361a206fe97829641269ff35e3e419dfdbeaa
Isaac Vaz <isaac.vaz@tecnico.ulisboa.pt>
2026-06-12 18:40:14 +0100

    full app done

---
+++ backend/main.py
@@ -0,0 +1,41 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Fruit(BaseModel):
+    name: str
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # The old tutorial port
+    "http://localhost:5173",  # Your new Vite local port
+    "http://127.0.0.1:5173",  # Your Vite loopback IP port
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {"fruits": []}
+
+@app.get("/fruits", response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits = memory_db["fruits"])
+
+@app.post("/fruits", response_model=Fruit)
+def add_fruit(fruit: Fruit):
+    memory_db["fruits"].append(fruit)
+    return fruit
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
